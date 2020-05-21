01b33bd198feecb6e579fc9033d576627605789a
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2020-05-21 11:06:28 -0500

    Do not use Popup

--- WinUIAccessLocationTest/WinUIAccessLocationTest/MainWindow.xaml.cs
+++ WinUIAccessLocationTest/WinUIAccessLocationTest/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using Windows.Devices.Geolocation;
 
 // The Blank Window item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -35,14 +25,7 @@ namespace WinUIAccessLocationTest
             {
                 var locator = new Geolocator();
                 var location = await locator.GetGeopositionAsync();
-                var popup = new Popup();
-                var contents = new TextBlock()
-                {
-                    Text = location.Coordinate.ToString(),
-                };
-                contents.PointerPressed += (s, e) => popup.IsOpen = false;
-                popup.Child = contents;
-                popup.IsOpen = true;
+                myText.Text = location.Coordinate.ToString();
             }
             myButton.Content = "Clicked";
         }
